6419edec2178eaa592b457365c46be5ac29b2c16
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2016-02-08 12:55:58 -0500

    Asociación de idiomas

---
+++ translation-service-api/src/main/java/co/edu/uniandes/csw/translationservice/dtos/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+package co.edu.uniandes.csw.translationservice.dtos;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
---
+++ translation-service-api/src/main/java/co/edu/uniandes/csw/translationservice/services/SkillService.java
@@ -0,0 +1,47 @@
+package co.edu.uniandes.csw.translationservice.services;
+
+import co.edu.uniandes.csw.auth.provider.StatusCreated;
+import co.edu.uniandes.csw.translationservice.api.ITranslatorLogic;
+import co.edu.uniandes.csw.translationservice.converters.LanguageConverter;
+import co.edu.uniandes.csw.translationservice.dtos.LanguageDTO;
+import co.edu.uniandes.csw.translationservice.entities.LanguageEntity;
+import static co.edu.uniandes.csw.translationservice.services.AccountService.getCurrentTranslator;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+
+@Path("skills")
+public class SkillService {
+
+    @Inject
+    private ITranslatorLogic translatorLogic;
+    @Context
+    private HttpServletRequest req;
+
+    @GET
+    public List<LanguageDTO> getLanguages() {
+        Long id = getCurrentTranslator(req.getRemoteUser()).getId();
+        return LanguageConverter.listEntity2DTO(translatorLogic.listLanguages(id));
+    }
+
+    @POST
+    @StatusCreated
+    @Path("{id: \\d+}")
+    public LanguageDTO createLanguage(@PathParam("id") Long languageId) {
+        Long translatorId = getCurrentTranslator(req.getRemoteUser()).getId();
+        return LanguageConverter.fullEntity2DTO(translatorLogic.addLanguages(translatorId, languageId));
+    }
+
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteLanguage(@PathParam("id") Long languageId) {
+        Long translatorId = getCurrentTranslator(req.getRemoteUser()).getId();
+        translatorLogic.removeLanguages(translatorId, languageId);
+    }
+}
